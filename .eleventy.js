b86ccc9f3fa07e2baad6cf698b1fec4c7640777e
Joe Lamyman <joe.lamyman@defra.gov.uk>
2022-02-02 16:28:59 +0000

    Fixed build issues

--- .eleventy.js
+++ .eleventy.js
@@ -3,12 +3,12 @@ const { DateTime } = require("luxon");
 module.exports = function(eleventyConfig) {
   // Return your Object options:
 
-  eleventyConfig.addCollection("blog", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("./src/blog/content/*.md");
+  eleventyConfig.addCollection("writing", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("./src/writing/content/*.md");
   });
 
   eleventyConfig.addCollection("recentPosts", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("./src/blog/content/*.md").reverse().slice(0, 3);
+    return collectionApi.getFilteredByGlob("./src/writing/content/*.md").reverse().slice(0, 3);
   });
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
@@ -18,6 +18,9 @@ module.exports = function(eleventyConfig) {
   // Copy across all images
   eleventyConfig.addPassthroughCopy("src/**/*.jpg");
   eleventyConfig.addPassthroughCopy("src/**/*.png");
+
+  // Copy across CSS in the documents folder
+  eleventyConfig.addPassthroughCopy("src/documents/**/*.css");
 
   // Copy across docx files
   eleventyConfig.addPassthroughCopy("src/**/*.docx");
